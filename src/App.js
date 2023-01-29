9421502e67cf1682d9083a3e6df3c04675f36fe8
abaker42 <anthony.baker390@gmail.com>
2023-01-28 22:10:48 -0500

    added new flyer and cashapp info

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import RegisterForm from './components/registerForm/RegisterForm';
 import children from './photos/children.jpg';
 import group from './photos/group.jpg';
 import little from './photos/littleone.jpg';
-import flyer from './photos/ClaudinaMasterClass_Jan2023.svg';
+import flyer from './photos/Claudina AdltClass_feb.svg';
 import ContactMe from './components/contactMe/ContactMe';
 
 function App() {
@@ -12,11 +12,20 @@ function App() {
 		<div className='App'>
 			<h1 className='App-Welcome'>Claudina's Ballet Classes</h1>
 			<div className='container'>
-				<img src={children} alt='' className='responsiveImg' />
-				<img src={group} alt='' className='responsiveImg' />
-				<img src={little} alt='' className='responsiveImg' />
+				<img src={children} alt='kids ballet class' className='responsiveImg' />
+				<img
+					src={group}
+					alt='group adult ballet class'
+					className='responsiveImg'
+				/>
+				<img
+					src={little}
+					alt='children train ballet dance'
+					className='responsiveImg'
+				/>
 			</div>
 			<h1 className='App-Welcome'>Register Now!</h1>
+			<p>Payments via cash or cashapp: $claudinapearl</p>
 			<RegisterForm />
 			<img src={flyer} alt='' className='balletFlyer' />
 			<h1 className='App-Welcome'>About the Director</h1>
